15e8b5bec70d09d3957490d84954ec0f2b1fbf06
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2024-04-17 13:15:59 +0200

    first commit

---
+++ fattoriale-iterativo.php
@@ -0,0 +1,26 @@
+<?php
+
+function fattorialeIter($num) {
+
+    $fatt = 1;
+
+    if ($num <= 0 ||$num == 1 ) {
+
+        return 1;
+
+    }
+
+    for ($i = 1; $i <= $num; $i++) {
+
+        $fatt *= $i;
+
+    }
+
+    return $fatt;
+
+}
+
+echo fattorialeIter(9);
+
+
+?>
